24a6ee5fe3ce233ee8a97d9ebe55568b2ba1c459
bt-uekat <bartlomiej.tomko@edu.uekat.pl>
2026-01-17 23:50:16 +0100

    rozwiązanie zadania 6

---
+++ 4. Operatory arytmetyczne, logiczne i typowanie/zad_6.py
@@ -0,0 +1,7 @@
+def process_lists(x: list, y: list) -> list:
+    unique_numbers = set(x + y)
+    cubed_numbers = []
+    for number in unique_numbers:
+        cubed_numbers.append(number ** 3)
+    return cubed_numbers
+print(process_lists([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]))
