650a57bba5680ccf74a4009d9a83ffecb9558619
bt-uekat <bartlomiej.tomko@edu.uekat.pl>
2026-01-18 23:18:39 +0100

    wersja 1 projektu

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+@app.get("/count-persons")
+def count_persons():
+    image_path = "test.jpg"
+
+    image = cv2.imread(image_path)
+
+    if image is None:
+        raise HTTPException(status_code=404, detail=f"Nie znaleziono pliku {image_path} na dysku.")
+
+    image = cv2.resize(image, (800, 600))
+
+    boxes, weights = hog.detectMultiScale(image, winStride=(8, 8), padding=(8, 8), scale=1.05)
+
+    person_count = len(boxes)
+
+    return {
+        "filename": image_path,
+        "persons_detected": person_count
+    }
