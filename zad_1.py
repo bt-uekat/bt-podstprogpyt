b0f0517cb59b52bf78155d6ae23c694fc02b3706
bt-uekat <bartlomiej.tomko@edu.uekat.pl>
2026-01-18 17:49:26 +0100

    dostosowanie kodu do PEP8

--- zad_1.py
+++ zad_1.py
@@ -1,4 +1,6 @@
 def print_names(names):
     for name in names:
         print(name)
+
+
 print_names(["Adam", "Bartek", "Cycylia", "Darek", "Ewa"])
--- zad_3.py
+++ zad_3.py
@@ -2,5 +2,7 @@ def print_evens(numbers):
     for number in numbers:
         if number % 2 == 0:
             print(number)
+
+
 numbers = range(10)
 print_evens(numbers)
