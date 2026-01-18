eee9fbd5e0b214485d8f634a5d849e669291b4e3
bt-uekat <bartlomiej.tomko@edu.uekat.pl>
2026-01-18 01:53:03 +0100

    rozwiązanie zadania 7

---
+++ zad_7.py
@@ -0,0 +1,22 @@
+import requests
+from typing import Optional
+class Brewery:
+    def __init__(self, name: str, city: str, brewery_type: str):
+        self.name: str = name
+        self.city: str = city
+        self.brewery_type: str = brewery_type
+    def __str__(self) -> str:
+        return f"Browar: {self.name} Miasto: {self.city} Typ: {self.brewery_type}"
+url = "https://api.openbrewerydb.org/v1/breweries"
+response = requests.get(url)
+data = response.json()
+brewery_list = []
+for item in data[:20]:
+    new_brewery = Brewery(
+        name=item.get("name", "Unknown"),
+        city=item.get("city", "Unknown"),
+        brewery_type=item.get("brewery_type", "Unknown"),
+    )
+    brewery_list.append(new_brewery)
+for brewery in brewery_list:
+    print(brewery)
