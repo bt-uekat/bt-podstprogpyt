49307d886dc0554a27c4ffed679857d5e6c14ba8
bt-uekat <bartlomiej.tomko@edu.uekat.pl>
2026-01-18 23:20:15 +0100

    wersja 1.1 projektu

--- main.py
+++ main.py
@@ -14,11 +14,17 @@ def count_persons():
     image = cv2.imread(image_path)
 
     if image is None:
-        raise HTTPException(status_code=404, detail=f"Nie znaleziono pliku {image_path} na dysku.")
+        raise HTTPException(
+            status_code=404,
+            detail=f"Nie znaleziono pliku {image_path} na dysku."
+        )
 
     image = cv2.resize(image, (800, 600))
 
-    boxes, weights = hog.detectMultiScale(image, winStride=(8, 8), padding=(8, 8), scale=1.05)
+    boxes, weights = hog.detectMultiScale(
+        image, winStride=(8, 8),
+        padding=(8, 8), scale=1.05
+    )
 
     person_count = len(boxes)
 
